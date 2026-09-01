939318e515b7b0c4fabc8dbb58532fd3065dbc18
nicofdez0408 <nicofdez0408@gmail.com>
2026-09-01 09:22:55 -0400

    Resolucion ejercicio 4 ts

---
+++ ejercicios-ts/ejercicio-4/app.js
@@ -0,0 +1,3 @@
+import { sumar } from "./suma.js";
+const resultado = sumar(4, 4);
+console.log(`El resultado de la suma es: ${resultado}`);
---
+++ ejercicios-ts/ejercicio-4/app.ts
@@ -0,0 +1,4 @@
+import { sumar } from "./suma.js";
+
+const resultado = sumar(4, 4);
+console.log(`El resultado de la suma es: ${resultado}`);
