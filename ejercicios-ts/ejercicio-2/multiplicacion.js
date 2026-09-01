3d9fc1070382e1adfbc8ef03243a1bc94f72a127
nicofdez0408 <nicofdez0408@gmail.com>
2026-09-01 09:10:34 -0400

    Resolucion ejercicio 2 ts

---
+++ ejercicios-ts/ejercicio-2/multiplicacion.js
@@ -0,0 +1,5 @@
+"use strict";
+function multiplicar(a, b) {
+    return a * b;
+}
+console.log("El resultado de la multiplicacion es: ", multiplicar(5, 4));
---
+++ ejercicios-ts/ejercicio-2/multiplicacion.ts
@@ -0,0 +1,5 @@
+function multiplicar(a: number, b: number): number {
+    return a * b;
+}
+
+console.log("El resultado de la multiplicacion es: ", multiplicar(5, 4));
